339aa51f6b1f21834fc31de8a20304b315b087e2
ELSON-341 <elson.sn314@gmail.com>
2024-04-15 15:00:30 -0300

    iniciando o js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,29 @@ const noteInput = document.querySelector('#note-content')
 const addNoteBtn = document.querySelector('.add-note')
 
 // funçãoes
+const addNote = () => {
+    const noteObject = {
+        id: createId(),
+        content:noteInput.value,
+        fixed: false,
+    }
+    const noteElement = createNote(noteObject.id, noteObject.content)
+    notesContainer.appendChild(noteElement)
+}
+const createId = () => Math.floor(Math.random() * 5000)
+
+const createNote = (id, content) => {
+    const Element = document.createElement('div')
+    Element.classList.add('note')
+
+    const textarea = document.createElement('textarea')
+    textarea.value = content
+    textarea.placeholder = "Adicione algum texto"
+    
+    Element.appendChild(textarea)
+
+    return Element
+}
 
 // Eventos
-addNoteBtn.addEventListener('click', () => {
-    console.log('test');
-})
+addNoteBtn.addEventListener('click', () => addNote())

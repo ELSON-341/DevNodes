6f7f14776531478fb158be6a59e4455b06357ee4
ELSON-341 <elson.sn314@gmail.com>
2024-04-16 15:10:36 -0300

    local storage

--- js/script.js
+++ js/script.js
@@ -4,18 +4,34 @@ const noteInput = document.querySelector('#note-content')
 const addNoteBtn = document.querySelector('.add-note')
 
 // funçãoes
+const showNotes = () => {
+    getNotes().forEach((note) => {
+        const noteElement = createNote(note.id, note.content, note.fixed)
+        notesContainer.appendChild(noteElement)
+    })
+}
+
 const addNote = () => {
+    const notes = getNotes()
+
     const noteObject = {
         id: createId(),
         content:noteInput.value,
         fixed: false,
     }
+
     const noteElement = createNote(noteObject.id, noteObject.content)
     notesContainer.appendChild(noteElement)
+
+    notes.push(noteObject)
+    
+    seveNotes(notes)
+    noteInput.value = ''
+    noteInput.focus()
 }
 const createId = () => Math.floor(Math.random() * 5000)
 
-const createNote = (id, content) => {
+const createNote = (id, content, fixed) => {
     const Element = document.createElement('div')
     Element.classList.add('note')
 
@@ -28,5 +44,19 @@ const createNote = (id, content) => {
     return Element
 }
 
+// Local storage
+const getNotes = () => {
+    const notes = JSON.parse(localStorage.getItem('notes') || "[]")
+    
+    return notes
+}
+
+const seveNotes = (notes) => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
 // Eventos
 addNoteBtn.addEventListener('click', () => addNote())
+
+// Inicialização
+showNotes()
